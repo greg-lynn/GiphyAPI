bb4bdc2a528bec695bf05a1f090e0c9bbe2f2d3b
Greg Lynn <greglynn1992@gmail.com>
2021-05-06 11:44:33 -0600

    adds comments

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//declares api key variable
 const apiKey = "i9u5FoDahfPMI2LFAipRD3KXJ45afk0f"
 async function giphySearch(event){
   event.preventDefault();
+  //variable 
   const giphyResultsDiv = document.getElementById('giphy-results')
+  //sets the value of the serach term
   const searchTerm = document.querySelector('input[name="search-term"]').value;
   const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}`);
   const giphyData = await response.json();
@@ -14,5 +17,5 @@ async function giphySearch(event){
     giphyResultsDiv.appendChild(image);
   }
 }
-
+//adds ecent listener to search form submit button
 document.querySelector("#search-form").addEventListener("submit", giphySearch)
